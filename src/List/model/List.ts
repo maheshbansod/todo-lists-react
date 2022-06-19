021f6da76d7384b3d379961a619bb474fba925d8
Mahesh Bansod <mahesh0bansod@gmail.com>
2022-06-19 20:45:07 +0530

    Made the list model use classes and added constructors

--- src/List/model/List.ts
+++ src/List/model/List.ts
@@ -1,14 +1,27 @@
+import { nanoid } from "@reduxjs/toolkit";
 
-export interface TodoList {
+export class TodoList {
     id: string; // list id
     title: string; // list title
     list: TodoItem[]; /// list of items
+
+    constructor(title: string, list: TodoItem[]) {
+        this.id = nanoid();
+        this.title = title;
+        this.list = list;
+    }
 }
 
-export interface TodoItem {
+export class TodoItem {
     id: string; // item id
     title: string; // item title
     status: TodoItemStatus; // item status
+
+    constructor(title: string) {
+        this.id = nanoid();
+        this.title = title;
+        this.status = 'initial';
+    }
 }
 
 type TodoItemStatus = 'done'|'ongoing'|'initial';
