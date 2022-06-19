c716b3704b138ec2067abffac29d2299acc24fdf
Mahesh Bansod <mahesh0bansod@gmail.com>
2022-06-19 20:39:46 +0530

    Moved List model to model folder and fixed import in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home/Home';
-import ListGroupView from './List/List';
-
-const listGroupIds = ["1", "2", "3"];
+import ListView from './List/List';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/list/:id" element={<ListGroupView />} />
+        <Route path="/list/:id" element={<ListView />} />
       </Routes>
     </BrowserRouter>
   );
