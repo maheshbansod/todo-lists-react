44b7c688054cf7cda34c46467c9e30197aa61169
Mahesh Bansod <mahesh0bansod@gmail.com>
2022-06-20 22:28:12 +0530

    Added new list component. modified new task fcomponent and fixed bugs

---
+++ src/List/NewList.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useAppDispatch } from "../hooks";
+import { listAdded } from "./listSlice";
+
+type Props = {
+    onListAdded?: (title: string)=>any;
+}
+
+// TODO: prevent duplicate lists??
+const NewListView = ({onListAdded}:Props) => {
+    const [title, setTitle] = useState("");
+
+    const dispatch = useAppDispatch();
+
+    const addList = () => {
+        dispatch(listAdded(title, []));
+        if(onListAdded)
+            onListAdded(title);
+    };
+
+    return <>
+        <div>
+            Title:
+            <input type="text"
+                placeholder="What do you want to call this list?"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+        </div>
+        <button onClick={addList}>Add</button>
+    </>
+};
+
+export default NewListView;
--- src/List/NewTask.tsx
+++ src/List/NewTask.tsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../hooks";
 import { selectLists } from "../store";
 import { listItemAdded } from "./listSlice";
+import NewListView from "./NewList";
 
 const NewTaskView = () => {
     const [title, setTitle] = useState("");
     const lists = useAppSelector(selectLists);
     const [list, setList] = useState(lists[0]);
+    const [listId, setListId] = useState("0");
+
+    const [showNewListView, setShowNewListView] = useState(false);
+
+    const NEW_LIST_OPTION_VALUE = "__new_list__";
 
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
@@ -16,6 +22,14 @@ const NewTaskView = () => {
         dispatch(listItemAdded(list, title));
         navigate(-1);
     };
+
+    useEffect(() => {
+        if(listId === NEW_LIST_OPTION_VALUE) {
+            const newList = lists[lists.length - 1];
+            setListId(newList.id);
+            setList(newList);
+        }
+    }, [lists]);
 
     return <>
         <div>
@@ -28,11 +42,27 @@ const NewTaskView = () => {
         </div>
         <div>
             List:
-            <select>
-                {lists.map(list => <option key={list.id} onClick={setList.bind(null,list)} value={list.id}>{list.title}</option>)}
+            <select value={listId} onChange={(evt)=>{
+                const { value } = evt.target;
+                setListId(value)
+                if(value === NEW_LIST_OPTION_VALUE) {
+                    setShowNewListView(true);
+                } else {
+                    setShowNewListView(false);
+                    const list = lists.find(list => list.id === value);
+                    if(list)
+                        setList(list);
+                }
+            }}>
+                {lists.map(list => <option key={list.id} value={list.id}>{list.title}</option>)}
+                <option value={NEW_LIST_OPTION_VALUE}>+ Add new list</option>
             </select>
         </div>
         <button onClick={addTodo}>Add</button>
+        {showNewListView && <div>
+                <NewListView onListAdded={setShowNewListView.bind(null, false)}/>
+                <button type="button" onClick={setShowNewListView.bind(null, false)}>Cancel</button>
+            </div>}
     </>
 };
 
